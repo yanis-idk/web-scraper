bfd94f23fd9d0f2aaed1347a9ae29769098993f6
yanis ouldlarbi <ouldlarbiyanis9@gmail.com>
2026-03-09 17:15:17 +0100

    added error handling and timeout

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from bs4 import BeautifulSoup
 data = []
 for page in range(1,11):
     url = f"https://quotes.toscrape.com/page/{page}/"
-    res = requests.get(url)
+    try:
+        res = requests.get(url,timeout=5)
+        res.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"Page {page} failed: {e}")
+        continue
     soup = BeautifulSoup(res.text,"html.parser")
     quotes = soup.find_all("span",class_="text")
     authors = soup.find_all("small",class_="author")
